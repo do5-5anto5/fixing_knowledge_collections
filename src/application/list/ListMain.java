c452bb8780d3cbdc510fd5a1bdaceae996ff2522
do5-5anto5 <tarraske.delico@gmail.com>
2023-08-25 23:43:49 -0300

    ListMain try-catch fix

--- src/application/list/ListMain.java
+++ src/application/list/ListMain.java
@@ -8,41 +8,32 @@ import services.ListService;
 public class ListMain {
 
 	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Type 3 task descriptions: ");	
-		
-		for (int i=0; i<3; i++) {
-			System.out.println("Adding task...");			
-			String taskInput = input.next();
-			
-			Task task = new Task(taskInput);		
-			ListService.addTask(task);			
-		}
-		
-		try {
-			
-		System.out.println("\nTasks number: ");
-		System.out.println(ListService.getNumberOfTasks());		
-				
-		System.out.print("\nEnter position to remove task: ");
-		String position = input.next();		
-		System.out.println("Removing '" + ListService.getTaskDescription(Integer.parseInt(position)) + "'");
-		ListService.removeTask(Integer.parseInt(position));
-		
-		System.out.println("\nTasks number: ");
-		System.out.println(ListService.getNumberOfTasks());		
 
-		}
-		catch (RuntimeException e) {
+		System.out.println("Type 3 task descriptions: ");
+
+		try (Scanner input = new Scanner(System.in)) {
+			for (int i = 0; i < 3; i++) {
+				System.out.println("Adding task...");
+				String taskInput = input.next();
+
+				Task task = new Task(taskInput);
+				ListService.addTask(task);
+			}
+
+			System.out.println("\nTasks number: ");
+			System.out.println(ListService.getNumberOfTasks());
+
+			System.out.print("\nEnter position to remove task: ");
+			String position = input.next();
+			System.out.println("Removing '" + ListService.getTaskDescription(Integer.parseInt(position)) + "'");
+			ListService.removeTask(Integer.parseInt(position));
+
+			System.out.println("\nTasks number: ");
+			System.out.println(ListService.getNumberOfTasks());
+
+		} catch (RuntimeException e) {
 			e.printStackTrace();
 		}
-		finally {
-		input.close();
-		}
 	}
-	
-	
 
 }
